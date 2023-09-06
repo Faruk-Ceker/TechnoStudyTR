f20bffa89f30153c9521babe920eb55ac8673832
Faruk-Ceker <farukceker.te@gmail.com>
2023-09-06 18:01:14 +0300

    US_07 kodlaması bitti ancak ana sayfaya dönüşlerde bug tespit edili

---
+++ src/MainProject/US_07_Faruk.java
@@ -0,0 +1,70 @@
+package MainProject;
+
+import Utility.BaseDriver;
+import org.apache.commons.io.FileUtils;
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+/*
+"Kendi Kursunu Seç" bölümünde her kursun altında "Detaylı Bilgi" veya "More Info" gibi bir buton veya bağlantı görünmelidir.
+
+"Detaylı Bilgi" butonuna tıkladığımda, ilgili kursun ayrıntılı bilgi ve içeriklerinin bulunduğu kurs sayfasına yönlendirilmelidir.
+
+İçeriklerin bulunduğu kursa sayfasında Techno Study logosu olmalıdır
+ */
+public class US_07_Faruk extends BaseDriver {
+
+    @Test
+    public void US_07_logoTest () throws AWTException, IOException {
+        driver.get("https://techno.study/tr");
+
+        String anaSayfailkURL = driver.getCurrentUrl();
+        List<WebElement> detayliBilgiler = driver.findElements(By.xpath("//a[text()='Detaylı bilgi']"));
+        Assert.assertTrue("Her kurs için detaylı bilgi butonu yok",detayliBilgiler.size()==3);
+
+        WebElement sdetBilgi = driver.findElement(By.xpath("(//a[text()='Detaylı bilgi'])[2]"));
+        sdetBilgi.click();
+        wait.until(ExpectedConditions.urlToBe("https://techno.study/tr/sdet"));
+        Assert.assertTrue("Hatalı kurs sayfasına yönlendirildi",driver.getCurrentUrl().contains("sdet"));
+
+        WebElement homePageLogo = driver.findElement(By.xpath("//img[@alt='TechnoStudy']"));
+        homePageLogo.click();
+
+        wait.until(ExpectedConditions.urlToBe("https://techno.study/home"));
+        String anaSayfaYeniURL = driver.getCurrentUrl();
+
+        if (anaSayfailkURL!=anaSayfaYeniURL) {
+            TakesScreenshot ts = (TakesScreenshot) driver;
+            File hafizadakiHali = ts.getScreenshotAs(OutputType.FILE);
+            FileUtils.copyFile(hafizadakiHali, new File("src\\ekranGoruntuleri\\screenshot_Faruk.png"));
+        }
+
+        WebElement androidBilgi = driver.findElement(By.xpath("(//a[text()='Detaylı bilgi'])[2]"));
+        androidBilgi.click();
+        wait.until(ExpectedConditions.urlToBe("https://techno.study/tr/androidapp"));
+        Assert.assertTrue("Hatalı kurs sayfasına yönlendirildi",driver.getCurrentUrl().contains("android"));
+
+
+        homePageLogo.click();
+
+
+        WebElement dataBilgi = driver.findElement(By.xpath("(//a[text()='Detaylı bilgi'])[1]"));
+        dataBilgi.click();
+        wait.until(ExpectedConditions.urlToBe("https://techno.study/tr/data"));
+        Assert.assertTrue("Hatalı kurs sayfasına yönlendirildi",driver.getCurrentUrl().contains("data"));
+
+        BekleVeKapat();
+    }
+}
