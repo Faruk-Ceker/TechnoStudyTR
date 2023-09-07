0c5350b21f973f54e64314eb74f350c31bb45948
LENOVO V15 Gen2 <ihsanahmedusmail@gmail.com>
2023-09-08 00:38:32 +0300

    US_05_Ihs test tamamlanmıştır.

--- src/MainProject/US_05_Ihs.java
+++ src/MainProject/US_05_Ihs.java
@@ -9,57 +9,33 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class US_05_Ihs extends BaseDriver {
 
     @Test
     public void sosyalMedyaErisimTesti (){
 
+       driver.get("https://techno.study/tr");
+       MyFunc.Wait(2);
 
-        driver.get("https://techno.study/tr");
-        MyFunc.Wait(2);
+        List<WebElement> linkler = driver.findElements(By.xpath("//div[@class='t-sociallinks']//a[@target='_blank']"));
+        for (WebElement e : linkler){
+            MyFunc.Wait(2);
+            e.click();
 
-        WebElement tecStudyTrFB = driver.findElement(By.xpath("//li[@class='t-sociallinks__item t-sociallinks__item_facebook']"));
-        tecStudyTrFB.click();
-        String x=driver.getCurrentUrl();
-        MyFunc.Wait(5);
-        System.out.println("x = " + x);
+        }
+        Set<String> windowsIdler = driver.getWindowHandles();
+        for (String id : windowsIdler) {
+            driver.switchTo().window(id);
+            System.out.println("title= "+driver.getTitle()+" -url= "+driver.getCurrentUrl());
+            Assert.assertTrue("URL 'techno' yazısını içermiyor" ,  driver.getCurrentUrl().toLowerCase().contains("techno"));
 
-
-        //switchToNewTab();
-        //wait.until(ExpectedConditions.urlContains("facebook"));
-        Assert.assertTrue("Current URL doesn't contain 'facebook'",  x.contains("facebook"));
-        closeTab();
-
-        WebElement tecStudyTrIns = driver.findElement(By.xpath("//li[@class='t-sociallinks__item t-sociallinks__item_instagram']"));
-        tecStudyTrIns.click();
-
-        switchToNewTab();
-        Assert.assertTrue("Current URL doesn't contain 'instagram'",  driver.getCurrentUrl().contains("instagram"));
-        closeTab();
-
-        WebElement tecStudyTrYB = driver.findElement(By.xpath("//li[@class='t-sociallinks__item t-sociallinks__item_youtube']"));
-        tecStudyTrYB.click();
-
-        switchToNewTab();
-        Assert.assertTrue("Current URL doesn't contain 'youtube'", driver.getCurrentUrl().contains("youtube"));
-        closeTab();
-
-        WebElement tecStudyTrLin = driver.findElement(By.xpath("//li[@class='t-sociallinks__item t-sociallinks__item_linkedin']"));
-        tecStudyTrLin.click();
-
-        switchToNewTab();
-        Assert.assertTrue("Current URL doesn't contain 'linkedin'",  driver.getCurrentUrl().contains("linkedin"));
-        closeTab();
+        }
 
         BekleVeKapat();
-
     }
 
-    private void closeTab() {
-    }
-
-    private void switchToNewTab() {
-    }
-
-}
+  }
